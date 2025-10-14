dd8943b897c0b592d7afbd4c7dbce055189d768e
Prin-S <98200861+Prin-S@users.noreply.github.com>
2025-10-14 23:00:59 +0700

    Added foreign keys to the games table.
    - Changed the order of CREATE TABLE because tables that are referenced have to be created first.
    - Likewise, changed the order of INSERT INTO so that data for referenced tables are inserted first.
    - Otherwise, non-existent values will be referenced, causing an error.

--- db/createDB.js
+++ db/createDB.js
@@ -2,36 +2,22 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const SQL = `
-  CREATE TABLE IF NOT EXISTS games (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR (100),
-    genre INTEGER,
-    developer INTEGER
-  );
-
   CREATE TABLE IF NOT EXISTS genres (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    genre_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     genre VARCHAR (100)
   );
 
   CREATE TABLE IF NOT EXISTS developers (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    developer_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     developer VARCHAR (100)
   );
 
-  INSERT INTO games (title, genre, developer) VALUES
-    ('Quake', 1, 1),
-    ('Quake II', 1, 1),
-    ('Age of Empires II', 2, 2),
-    ('Baldur''s Gate', 3, 3),
-    ('MDK2', 4, 3),
-    ('Neverwinter Nights', 3, 3),
-    ('Mass Effect', 3, 3),
-    ('Myst', 5, 4),
-    ('Riven', 5, 4),
-    ('Halo', 1, 5),
-    ('Age of Wonders', 6, 6),
-    ('Heroes of Might and Magic III', 6, 7);
+  CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR (100),
+    genre_id INTEGER REFERENCES genres,
+    developer_id INTEGER REFERENCES developers
+  );
 
   INSERT INTO genres (genre) VALUES
     ('First-person shooter'),
@@ -49,6 +35,20 @@ const SQL = `
     ('Bungie'),
     ('Triumph Studios'),
     ('New World Computing');
+  
+  INSERT INTO games (title, genre_id, developer_id) VALUES
+    ('Quake', 1, 1),
+    ('Quake II', 1, 1),
+    ('Age of Empires II', 2, 2),
+    ('Baldur''s Gate', 3, 3),
+    ('MDK2', 4, 3),
+    ('Neverwinter Nights', 3, 3),
+    ('Mass Effect', 3, 3),
+    ('Myst', 5, 4),
+    ('Riven', 5, 4),
+    ('Halo', 1, 5),
+    ('Age of Wonders', 6, 6),
+    ('Heroes of Might and Magic III', 6, 7);
 `;
 
 async function main() {
