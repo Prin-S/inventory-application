1e8e3b3b7fd91bba69e56009774ebe6e2707f28c
Prin-S <98200861+Prin-S@users.noreply.github.com>
2025-10-15 15:29:45 +0700

    Added DB get queries.

---
+++ db/getQueries.js
@@ -0,0 +1,39 @@
+const pool = require('./pool');
+
+async function getAllGamesFromDB() {
+  const rows = await pool.query(`SELECT games.id, games.title, genres.genre, developers.developer
+    FROM games
+    JOIN genres ON games.genre_id = genres.genre_id
+    JOIN developers ON games.developer_id = developers.developer_id`);
+  return rows.rows;
+}
+
+async function getAllGenresFromDB() {
+  const rows = await pool.query('SELECT * FROM genres');
+  return rows.rows;
+}
+
+async function getAllDevelopersFromDB() {
+  const rows = await pool.query('SELECT * FROM developers');
+  return rows.rows;
+}
+
+async function getSingleGenreFromDB(id) {
+  const rows = await pool.query(`SELECT games.id, games.title, genres.genre, developers.developer
+    FROM games
+    JOIN genres ON games.genre_id = genres.genre_id
+    JOIN developers ON games.developer_id = developers.developer_id
+    WHERE genres.genre_id = ($1)`, [id]);
+  return rows.rows;
+}
+
+async function getSingleDeveloperFromDB(id) {
+  const rows = await pool.query(`SELECT games.id, games.title, genres.genre, developers.developer
+    FROM games
+    JOIN genres ON games.genre_id = genres.genre_id
+    JOIN developers ON games.developer_id = developers.developer_id
+    WHERE developers.developer_id = ($1)`, [id]);
+  return rows.rows;
+}
+
+module.exports = { getAllGamesFromDB, getAllGenresFromDB, getAllDevelopersFromDB, getSingleGenreFromDB, getSingleDeveloperFromDB };
