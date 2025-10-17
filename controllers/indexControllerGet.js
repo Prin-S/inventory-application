042bb623e54b3e677aec7a03440ffae1d0710eb5
Prin-S <98200861+Prin-S@users.noreply.github.com>
2025-10-17 21:48:08 +0700

    Fixed the message when a genre/developer is empty.
    "No games found for this genre/developer" is shown instead of a 404 page.

--- controllers/indexControllerGet.js
+++ controllers/indexControllerGet.js
@@ -4,48 +4,65 @@ const links = require('../links');
 async function getAllGames(req, res) {
   const title = 'All Games';
   const games = await db.getAllGamesFromDB();
+  const type = 'none';
 
-  res.render('index', { title, links, games });
+  res.render('index', { title, links, type, games });
 }
 
 async function getAllGenres(req, res) {
   const title = 'All Genres';
   const genres = await db.getAllGenresFromDB();
+  const type = 'none';
 
-  res.render('index', { title, links, genres });
+  res.render('index', { title, links, type, genres });
 }
 
 async function getAllDevelopers(req, res) {
   const title = 'All Developers';
   const developers = await db.getAllDevelopersFromDB();
+  const type = 'none';
 
-  res.render('index', { title, links, developers });
+  res.render('index', { title, links, type, developers });
 }
 
 async function getSingleGenre(req, res) {
   const genreID = req.params.genre_id;
-  const genreEntries = await db.getSingleGenreFromDB(genreID);
-  
-  if (genreEntries.length == 0) {
+  const genres = await db.getAllGenresFromDB();
+  const selectedGenre = genres.find(genre => genre.genre_id == genreID);
+
+  if (!selectedGenre) {
     throw new Error('Genre');
   }
 
-  const type = 'Genre';
-  const title = genreEntries[0].genre;  
+  const title = selectedGenre.genre;
+  const genreEntries = await db.getSingleGenreFromDB(genreID);
+  
+  if (genreEntries.length == 0) {
+    type = 'empty genre';
+  } else {
+    type = 'genre';
+  }
   
   res.render('index', { title, links, type, genreEntries });
 }
 
 async function getSingleDeveloper(req, res) {
   const developerID = req.params.developer_id;
+  const developers = await db.getAllDevelopersFromDB();
+  const selectedDeveloper = developers.find(developer => developer.developer_id == developerID);
+
+  if (!selectedDeveloper) {
+    throw new Error('Developer');
+  }
+  
+  const title = selectedDeveloper.developer;
   const developerEntries = await db.getSingleDeveloperFromDB(developerID);
   
   if (developerEntries.length == 0) {
-    throw new Error('Developer');
+    type = 'empty developer';
+  } else {
+    type = 'developer';
   }
-
-  const type = 'Developer';
-  const title = developerEntries[0].developer;  
   
   res.render('index', { title, links, type, developerEntries });
 }
